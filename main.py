22ef6fd13d599459299deb72b821b7ab928b940c
yanbo <10955940+yanbostudio@users.noreply.github.com>
2022-05-06 15:09:11 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+FEISHU_WEBHOOK_URL = "https://open.feishu.cn/open-apis/bot/v2/hook/xxxxxxxxxxxxx"
+
+
+def feishu_alter(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+
+    request_json = request.get_json()
+    if request.args and 'message' in request.args:
+        message = request.args.get('message')
+    elif request_json and 'message' in request_json:
+        message = request_json['message']
+    else:
+        message = f'no message'
+
+    payload_message = {
+        "msg_type": "text",
+        "content": {
+            "text": "标题：{0} \n消息：{1}".format("GCP 告警", message)}
+    }
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    res = requests.request("POST", FEISHU_WEBHOOK_URL, headers=headers, data=json.dumps(payload_message))
+
+    return f'ok'
